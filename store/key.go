81597131886ab6aa9a080f88bc1554168c11e39a
Konstantin Falko <kotfalya@gmail.com>
2016-09-26 21:12:22 +0300

    refactoring

--- store/key.go
+++ store/key.go
@@ -1,5 +1,9 @@
 package store
 
+var (
+	_ CmdRes = (*StrCmdRes)(nil)
+)
+
 type Key interface {
 	Name() string
 	Enabled() bool
@@ -18,4 +22,66 @@ func (bk *BaseKey) Name() string {
 
 func (bk *BaseKey) Enabled() bool {
 	return bk.enabled
+}
+
+type Cmd interface {
+	Process(st *Store)
+	Res() CmdRes
+	SetRes(res CmdRes)
+}
+
+type BaseCmd struct {
+	res CmdRes
+}
+
+func (bc *BaseCmd) Res() CmdRes {
+	return bc.res
+}
+
+func (bc *BaseCmd) SetRes(res CmdRes) {
+	bc.res = res
+}
+
+type CmdRes interface {
+	Err() error
+}
+
+type StrCmdRes struct {
+	err error
+	val string
+}
+
+func NewStrCmdRes(val string, err error) *StrCmdRes {
+	return &StrCmdRes{
+		val: val,
+		err: err,
+	}
+}
+
+func (scr *StrCmdRes) Err() error {
+	return scr.err
+}
+
+func (scr *StrCmdRes) Val() string {
+	return scr.val
+}
+
+type BoolCmdRes struct {
+	err error
+	val bool
+}
+
+func NewBoolCmdRes(val bool, err error) *BoolCmdRes {
+	return &BoolCmdRes{
+		val: val,
+		err: err,
+	}
+}
+
+func (scr *BoolCmdRes) Err() error {
+	return scr.err
+}
+
+func (scr *BoolCmdRes) Val() bool {
+	return scr.val
 }
--- store/store.go
+++ store/store.go
@@ -25,3 +25,21 @@ func (s *Store) AddKey(key Key) error {
 
 	return res.Err()
 }
+
+func (s *Store) Get(keyName string) (string, error) {
+	cmd := NewGetCmd(keyName)
+	cmd.Process(s)
+
+	res := cmd.Res().(*StrCmdRes)
+
+	return res.Val(), res.Err()
+}
+
+func (s *Store) Set(keyName string, value string) (bool, error) {
+	cmd := NewSetCmd(keyName, value)
+	cmd.Process(s)
+
+	res := cmd.Res().(*BoolCmdRes)
+
+	return res.Val(), res.Err()
+}
